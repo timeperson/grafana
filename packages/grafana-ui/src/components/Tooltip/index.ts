90d0e2fd4db0b01643775b795bc9c17a317c841f
lvshaobo <lvshaobo@cmict.chinamobile.com>
2024-01-04 20:42:09 +0800

    add

--- packages/grafana-ui/src/components/Tooltip/index.ts
+++
@@ -1,3 +0,0 @@
-export { Tooltip, TooltipProps } from './Tooltip';
-export { PopoverController } from './PopoverController';
-export { TooltipPlacement, PopoverContent, PopoverContentProps } from './types';
